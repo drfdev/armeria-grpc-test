ca4b9f5455a8049d6945ecc07cb70f85a24c3ea5
drfdev <drafff@gmail.com>
2020-06-22 08:32:47 +0300

    add spring boot

---
+++ grpc-armeria-service/src/main/java/dev/drf/grpc/arm/service/Application.java
@@ -0,0 +1,13 @@
+package dev.drf.grpc.arm.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource({"classpath*:applicationContext.xml"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
